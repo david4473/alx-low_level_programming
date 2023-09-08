a183e648bced991a87032d0b5d4a8d9a75563441
david4473 <omotayodavid9@gmail.com>
2023-09-08 06:49:46 -0700

    Automated commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar(i);
+	return (0);
+}
