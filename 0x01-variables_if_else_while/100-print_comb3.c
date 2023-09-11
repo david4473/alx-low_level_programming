571c2757a5dad3be2bca2ea12f4ffbc8e0bf1626
david4473 <omotayodavid9@gmail.com>
2023-09-11 08:56:42 -0700

    Automated commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 57; i++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 56 && j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
