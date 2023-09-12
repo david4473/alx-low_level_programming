92e9298fe16f89e43b3b6f80badd551b2f250bbc
david4473 <omotayodavid9@gmail.com>
2023-09-12 15:55:33 -0700

    Automated commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints time
+ *
+ *Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+	}
+
+}
