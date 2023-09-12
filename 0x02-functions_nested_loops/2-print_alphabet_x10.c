483e2a443cfe8bd1f1823794118c65fb8c817e87
david4473 <omotayodavid9@gmail.com>
2023-09-12 05:19:39 -0700

    Automated commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print alphabet x10 - prints a - z 10 times
+ *Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
