bfff91dd25de4595f6d63c873a72b9ff5f554e86
david4473 <omotayodavid9@gmail.com>
2023-09-12 04:52:56 -0700

    Automated commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabet a - z
+ *Return: Always 0 (success)
+ */
+
+void print_alphabet (void)
+{
+	char i;
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
