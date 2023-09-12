cd68ce73ffd21c9fafc6a41180c826f425296058
david4473 <omotayodavid9@gmail.com>
2023-09-12 15:01:44 -0700

    Automated commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: arguement to be returned
+ *Return: always 0
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = n % 10;
+
+	return (lastDigit);
+}
