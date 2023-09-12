7b2028e12dca94edc8a2d241157fc71696b27cdd
david4473 <omotayodavid9@gmail.com>
2023-09-12 04:54:59 -0700

    Automated commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,14 @@
  *Return: Always 0 (success)
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i < 'z'; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+
+	_putchar('\n');
 }
