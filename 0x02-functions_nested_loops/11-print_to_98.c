69b64c4ca99ed4d8519c3ebcc790b907c54813c3
david4473 <omotayodavid9@gmail.com>
2023-09-13 13:29:58 -0700

    Automated commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural number to 98
+ *@n: arguement
+ *Return 0
+ */
+
+void print_to_98(int n)
+{
+
+	int i;
+
+	if (n > 98)
+	{
+
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
