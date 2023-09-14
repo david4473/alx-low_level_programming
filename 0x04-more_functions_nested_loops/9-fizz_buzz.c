b913681f3990578e86e4f71a4f113e76965d1ce7
david4473 <omotayodavid9@gmail.com>
2023-09-14 14:53:33 -0700

    Automated commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 15 == 0)
-			printf("FizzBuzz");
 		else
-			printf("%d", i);
+			printf("%i", i);
 
 		if (i != 100)
 			printf(" ");
