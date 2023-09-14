20d63286423f2bfd09f17cafc3fdc8780dcfaaf4
david4473 <omotayodavid9@gmail.com>
2023-09-14 11:35:36 -0700

    Automated commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 4 || i != 2)
+		if (!(i == '2' || i == '4'))
 		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 
 		_putchar('\n');
