431a0ce85f8165ae50c8323f1f631044e59d8aaf
david4473 <omotayodavid9@gmail.com>
2023-09-14 11:09:04 -0700

    Automated commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints number 0 to 9
+ *
+ *Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
