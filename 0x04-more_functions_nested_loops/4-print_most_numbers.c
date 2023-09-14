ed8f637c6d390ac172406b3fef7c997616526da3
david4473 <omotayodavid9@gmail.com>
2023-09-14 11:14:13 -0700

    Automated commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - prints numbers 0 to 9 with an exception of 4 and 2
+ *
+ *Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 52 && i == 50)
+		{
+			_putchar(i + '0');
+		}
+	}
+}
