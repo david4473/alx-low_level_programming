3e54cb900254c8ebd46fecf9ff95333c80908ac1
david4473 <omotayodavid9@gmail.com>
2023-09-19 01:14:12 -0700

    Automated commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other char
+ *@str: argument
+ *Return: always 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
