e6fea3239f78cb378e3ce42a11e6da3c5ddcb3ec
david4473 <omotayodavid9@gmail.com>
2023-09-18 21:50:59 -0700

    Automated commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - returns lenght of string
+ *@s: argument
+ *Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	return lenght;
+}
