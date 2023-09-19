5f71d885dbdaf3263f0d05594add4c5240e601a6
david4473 <omotayodavid9@gmail.com>
2023-09-19 02:23:29 -0700

    Automated commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 
 /**
  *print_array - prints an array
@@ -16,9 +15,11 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != (n -1))
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
 	}
+
+	putchar('\n');
 }
