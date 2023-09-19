bb330d615c44e6bd7ae65e09a1056701b9d05186
david4473 <omotayodavid9@gmail.com>
2023-09-19 01:05:11 -0700

    Automated commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: argument
+ *Return: always 0
+ */
+
+void rev_string(char *s)
+{
+	int i, j, u;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	j = 0;
+	u = i / 2;
+
+	while (u--)
+	{
+		tmp = s[i - j - 1];
+		s[i - j - 1] = s[j];
+		s[j] = tmp;
+		j++;
+	}
+}
